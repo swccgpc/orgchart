405a5419435c116f73a35780b7ce1f8c9c5f6f15
Devon Hubner <devon@hubner.org>
2021-02-19 21:02:30 -0500

    pdf policy update

--- make_jpg.sh
+++ make_jpg.sh
@@ -6,6 +6,15 @@ if [ "x$(which convert)" == "x" ]; then
   echo
   exit 1
 else
+
+  if [ -f /etc/ImageMagick-6/policy.xml ]; then
+    sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
+  fi
+
+  if [ -f /etc/ImageMagick-7/policy.xml ]; then
+    sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
+  fi
+
   convert orgchart.pdf -flatten -fuzz 1% -trim +repage orgchart.jpg
 fi
 
