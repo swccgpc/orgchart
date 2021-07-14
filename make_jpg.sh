2962d47c2664c3143e09144bdc47d2f4ddc17222
Devon Hubner <devon@hubner.org>
2021-07-14 11:47:55 -0400

    updated make_jpg.sh script

--- make_jpg.sh
+++ make_jpg.sh
@@ -8,14 +8,25 @@ if [ "x$(which convert)" == "x" ]; then
 else
 
   if [ -f /etc/ImageMagick-6/policy.xml ]; then
+    echo
+    echo "/etc/ImageMagick-6/policy.xml"
+    ls -al /etc/ImageMagick-6/policy.xml
     sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
   fi
 
   if [ -f /etc/ImageMagick-7/policy.xml ]; then
-    sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
+    echo
+    echo "/etc/ImageMagick-7/policy.xml"
+    sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-7/policy.xml
   fi
 
+  echo
+  echo "converting orgchart from pdf to jpg"
   convert orgchart.pdf -flatten -fuzz 1% -trim +repage orgchart.jpg
+
+  echo
+  echo "done."
+  echo
 fi
 
 exit 0
