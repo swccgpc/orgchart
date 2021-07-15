ac7d9ee231180d702dcb935734a10af898cb3bf9
Devon Hubner (AWS) <dhhubner@amazon.com>
2021-07-14 20:28:51 -0400

    make jpg updates

--- make_jpg.sh
+++ make_jpg.sh
@@ -2,37 +2,44 @@
 
 if [ "x$(which convert)" == "x" ]; then
   echo
-  echo "Convert not available. Please Install ImageMagick."
+  echo "Convert not available. Installing ImageMagick."
   echo
-  exit 1
-else
-
-  export SUDO_CMD=$(which sudo)
-  echo
-  echo "sudo: $SUDO_CMD"
-  echo
-
-  if [ -f /etc/ImageMagick-6/policy.xml ]; then
-    echo
-    echo "/etc/ImageMagick-6/policy.xml"
-    ls -al /etc/ImageMagick-6/policy.xml
-    $SUDO_CMD sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
-  fi
-
-  if [ -f /etc/ImageMagick-7/policy.xml ]; then
-    echo
-    echo "/etc/ImageMagick-7/policy.xml"
-    $SUDO_CMD sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-7/policy.xml
-  fi
-
-  echo
-  echo "converting orgchart from pdf to jpg"
-  convert orgchart.pdf -flatten -fuzz 1% -trim +repage orgchart.jpg
-
-  echo
-  echo "done."
-  echo
+  sudo apt-get install -y imagemagick
 fi
+
+if [ "x$(which gs)" == "x" ]; then
+  echo
+  echo "gs not available. Installing Ghostscript."
+  echo
+  sudo apt-get install -y ghostscript
+fi
+
+export SUDO_CMD=$(which sudo)
+echo
+echo "sudo: $SUDO_CMD"
+echo
+
+if [ -f /etc/ImageMagick-6/policy.xml ]; then
+  echo
+  echo "/etc/ImageMagick-6/policy.xml"
+  ls -al /etc/ImageMagick-6/policy.xml
+  $SUDO_CMD sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-6/policy.xml
+fi
+
+if [ -f /etc/ImageMagick-7/policy.xml ]; then
+  echo
+  echo "/etc/ImageMagick-7/policy.xml"
+  $SUDO_CMD sed -i 's/rights="none" pattern="PDF"/rights="read | write" pattern="PDF"/' /etc/ImageMagick-7/policy.xml
+fi
+
+echo
+echo "converting orgchart from pdf to jpg"
+convert orgchart.pdf -flatten -fuzz 1% -trim +repage orgchart.jpg
+
+echo
+echo "done."
+echo
+
 
 exit 0
 
